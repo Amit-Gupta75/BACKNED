2a8dc5199cf3e7ee3e45adbf6f876ed15b724eac
Amit Gupta <guptaamit7571@gmai.com>
2026-01-25 21:33:35 +0530

    some bugs fix

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-      (req,res,next) => {
+     return (req,res,next) => {
         Promise.resolve(requestHandler(req,res,next)).
         catch((err) => next(err))
       }
@@ -10,11 +10,11 @@ export {asyncHandler}
 /*
 const asyncHandler = (fn) => async (req,res,next) => {
     try {
-       await fu(req,res,next) 
+       await fn(req,res,next) 
     } catch (error) {
       res.status(error.code || 500).json({
         success: false,
-        message: err.message
+        message: error.message
       })  
     }
 }
