2a7ec004b924d132b90f8dc0c08a520cc438eefc
Amit Gupta <guptaamit7571@gmai.com>
2026-01-26 12:04:42 +0530

    All setup configure

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+console.log("APP FILE LOADED");
+
+
 const app = express()
 
 app.use(cors({
@@ -13,6 +16,18 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+// routes import
+
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration 
+
+app.use("/api/v1/users", userRouter)
+
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import dotenv from "dotenv"
 //  import { DB_NAME } from "./constants";
  import connectDB from "./db/index.js";
  import express from "express"
+ import { app } from "./app.js";
 
- const app = express()
+//  const app = express()
 
  dotenv.config({
    path: './env'
